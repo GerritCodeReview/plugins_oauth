2034a3e9b40c5ee92a43d3d035dfe47115693352
Thomas Dräbing <thomas.draebing@sap.com>
2025-01-24 14:30:47 +0100

    Add basic OAuth support for GitOverHttp to SAP IAS
    This allows to login with an OAuth token sent as a password.
    
    Change-Id: Iaec21b0e23b061601bc3e78aad72fca003be7672

---
+++ src/main/java/com/googlesource/gerrit/plugins/oauth/SAPIasOAuthLoginProvider.java
@@ -0,0 +1,41 @@
+// Copyright (C) 2025 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.oauth;
+
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.google.gerrit.extensions.auth.oauth.OAuthLoginProvider;
+import com.google.gerrit.extensions.auth.oauth.OAuthUserInfo;
+import com.google.inject.Inject;
+import com.google.inject.Singleton;
+import java.io.IOException;
+
+@Singleton
+class SAPIasOAuthLoginProvider implements OAuthLoginProvider {
+  private final SAPIasOAuthService service;
+
+  @Inject
+  SAPIasOAuthLoginProvider(SAPIasOAuthService service) {
+    this.service = service;
+  }
+
+  @Override
+  public OAuthUserInfo login(String username, String token) throws IOException {
+    if (username == null || token == null) {
+      throw new IOException("Authentication error");
+    }
+    OAuth2AccessToken accessToken = new OAuth2AccessToken(token);
+    return service.getUserInfo(accessToken);
+  }
+}
