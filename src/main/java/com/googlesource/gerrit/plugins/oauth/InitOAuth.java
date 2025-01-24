f3a528a8df43adfca59ddcf45f4b59b9a6ddd6a7
Thomas Dräbing <thomas.draebing@sap.com>
2025-01-24 15:36:07 +0100

    Add optional support for PKCE in SAP IAS
    Change-Id: I3fcaedd58266fb7bf7aeef5274e14e16c8cffe25

--- src/main/java/com/googlesource/gerrit/plugins/oauth/InitOAuth.java
+++ src/main/java/com/googlesource/gerrit/plugins/oauth/InitOAuth.java
@@ -28,6 +28,7 @@ class InitOAuth implements InitStep {
   static final String PLUGIN_SECTION = "plugin";
   static final String CLIENT_ID = "client-id";
   static final String CLIENT_SECRET = "client-secret";
+  static final String ENABLE_PKCE = "enable-pkce";
   static final String LINK_TO_EXISTING_OPENID_ACCOUNT = "link-to-existing-openid-accounts";
   static final String FIX_LEGACY_USER_ID = "fix-legacy-user-id";
   static final String DOMAIN = "domain";
@@ -160,6 +161,8 @@ class InitOAuth implements InitStep {
             isConfigured(iasOAuthProviderSection), "Use SAP IAS OAuth provider for Gerrit login ?");
     if (configureIASOAuthProvider && configureOAuth(iasOAuthProviderSection)) {
       checkRootUrl(iasOAuthProviderSection.string("SAP IAS Root URL", ROOT_URL, null));
+      iasOAuthProviderSection.string(
+          "Enable PKCE for SAP IAS OAuth provider?", ENABLE_PKCE, "false");
     }
 
     boolean configureLemonLDAPOAuthProvider =
