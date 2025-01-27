0949fe628eab412566ec74653d5d9bd617d1dcd3
Thomas Dräbing <thomas.draebing@sap.com>
2025-01-27 12:18:41 +0100

    [SAP IAS] Verify token on login
    This is heavily inspired by the cfoauth plugin.
    
    Change-Id: I9c0752d2e62ecb438ab013cb5fd9f4e30f92d9d4

--- src/main/java/com/googlesource/gerrit/plugins/oauth/SAPIasOAuthLoginProvider.java
+++ src/main/java/com/googlesource/gerrit/plugins/oauth/SAPIasOAuthLoginProvider.java
@@ -14,15 +14,24 @@
 
 package com.googlesource.gerrit.plugins.oauth;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.github.scribejava.core.model.OAuth2AccessToken;
 import com.google.gerrit.extensions.auth.oauth.OAuthLoginProvider;
 import com.google.gerrit.extensions.auth.oauth.OAuthUserInfo;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 import java.io.IOException;
+import java.util.Base64;
 
 @Singleton
 class SAPIasOAuthLoginProvider implements OAuthLoginProvider {
+  private static final String USER_NAME_ATTRIBUTE = "sub";
+
   private final SAPIasOAuthService service;
 
   @Inject
@@ -32,10 +41,57 @@ class SAPIasOAuthLoginProvider implements OAuthLoginProvider {
 
   @Override
   public OAuthUserInfo login(String username, String token) throws IOException {
-    if (username == null || token == null) {
+    if (username == null || token == null || !isAccessToken(token)) {
       throw new IOException("Authentication error");
     }
     OAuth2AccessToken accessToken = new OAuth2AccessToken(token);
     return service.getUserInfo(accessToken);
+  }
+
+  private boolean isAccessToken(String accessToken) {
+    try {
+      JsonObject jsonWebToken = toJsonWebToken(accessToken);
+      return getAttribute(jsonWebToken, USER_NAME_ATTRIBUTE) != null;
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+  private static String getAttribute(JsonObject json, String name) {
+    JsonPrimitive prim = getAsJsonPrimitive(json, name);
+    return prim != null && prim.isString() ? prim.getAsString() : null;
+  }
+
+  private static JsonPrimitive getAsJsonPrimitive(JsonObject json, String name) {
+    JsonElement attr = json.get(name);
+    if (attr == null || !attr.isJsonPrimitive()) {
+      return null;
+    }
+    return attr.getAsJsonPrimitive();
+  }
+
+  public static JsonObject getAsJsonObject(String s) {
+    JsonElement json = JsonParser.parseString(s);
+    if (!json.isJsonObject()) {
+      return new JsonObject();
+    }
+    return json.getAsJsonObject();
+  }
+
+  private JsonObject toJsonWebToken(String accessToken) throws IOException {
+    String[] segments = getSegments(accessToken);
+    return getAsJsonObject(decodeBase64(segments[1]));
+  }
+
+  private String decodeBase64(String s) {
+    return new String(Base64.getDecoder().decode(s), UTF_8);
+  }
+
+  private String[] getSegments(String accessToken) throws IOException {
+    String[] segments = accessToken.split("\\.");
+    if (segments.length != 3) {
+      throw new IOException("Invalid token: must be of the form 'header.token.signature'");
+    }
+    return segments;
   }
 }
