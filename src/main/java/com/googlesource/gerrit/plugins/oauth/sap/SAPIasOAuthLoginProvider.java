0fa11f3972a1e2030cfc76a0397c5f37e072b55e
Thomas Dräbing <thomas.draebing@sap.com>
2025-01-27 12:18:41 +0100

    [SAP IAS] Optionally allow the Resource Owner Password Flow
    Change-Id: If8a57ac4d487fece946b76a49fddeba3d63bc192

--- src/main/java/com/googlesource/gerrit/plugins/oauth/sap/SAPIasOAuthLoginProvider.java
+++ src/main/java/com/googlesource/gerrit/plugins/oauth/sap/SAPIasOAuthLoginProvider.java
@@ -14,30 +14,132 @@
 
 package com.googlesource.gerrit.plugins.oauth.sap;
 
+import static com.google.gerrit.server.account.externalids.ExternalId.SCHEME_USERNAME;
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.google.gerrit.entities.Account;
 import com.google.gerrit.extensions.auth.oauth.OAuthLoginProvider;
 import com.google.gerrit.extensions.auth.oauth.OAuthUserInfo;
+import com.google.gerrit.server.account.externalids.ExternalId;
+import com.google.gerrit.server.account.externalids.ExternalIdKeyFactory;
+import com.google.gerrit.server.account.externalids.ExternalIds;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonPrimitive;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
+import com.googlesource.gerrit.plugins.oauth.OAuthPluginConfigFactory;
 import com.googlesource.gerrit.plugins.oauth.OAuthServiceProviderConfig;
+import com.googlesource.gerrit.plugins.oauth.OAuthServiceProviderExternalIdScheme;
 import java.io.IOException;
+import java.util.Base64;
+import java.util.Optional;
 
 @Singleton
 @OAuthServiceProviderConfig(name = SAPIasOAuthService.PROVIDER_NAME)
 public class SAPIasOAuthLoginProvider implements OAuthLoginProvider {
+  private static final String USER_NAME_ATTRIBUTE = "sub";
+
   private final SAPIasOAuthService service;
+  private final boolean enableResourceOwnerPasswordFlow;
+  private final ExternalIds externalIds;
+  private final ExternalIdKeyFactory externalIdKeyFactory;
+  private final String extIdScheme;
 
   @Inject
-  SAPIasOAuthLoginProvider(SAPIasOAuthService service) {
+  SAPIasOAuthLoginProvider(
+      SAPIasOAuthService service,
+      OAuthPluginConfigFactory cfgFactory,
+      ExternalIds externalIds,
+      ExternalIdKeyFactory externalIdKeyFactory) {
     this.service = service;
+    this.enableResourceOwnerPasswordFlow =
+        cfgFactory
+            .create(SAPIasOAuthService.PROVIDER_NAME)
+            .getBoolean("enableResourceOwnerPasswordFlow", false);
+    this.externalIds = externalIds;
+    this.externalIdKeyFactory = externalIdKeyFactory;
+    this.extIdScheme =
+        OAuthServiceProviderExternalIdScheme.create(SAPIasOAuthService.PROVIDER_NAME);
   }
 
   @Override
-  public OAuthUserInfo login(String username, String token) throws IOException {
-    if (username == null || token == null) {
+  public OAuthUserInfo login(String username, String secret) throws IOException {
+    if (secret == null) {
       throw new IOException("Authentication error");
     }
-    OAuth2AccessToken accessToken = new OAuth2AccessToken(token);
+    OAuth2AccessToken accessToken;
+    if (isAccessToken(secret)) {
+      accessToken = new OAuth2AccessToken(secret);
+    } else if (enableResourceOwnerPasswordFlow) {
+      if (username == null) {
+        throw new IOException("Authentication error");
+      }
+      Optional<Account.Id> accountId =
+          externalIds
+              .get(externalIdKeyFactory.create(SCHEME_USERNAME, username))
+              .map(ExternalId::accountId);
+      if (accountId.isEmpty()) {
+        throw new IOException("Authentication error");
+      }
+      ExternalId extId =
+          externalIds.byAccount(accountId.get()).stream()
+              .filter(e -> e.key().isScheme(this.extIdScheme))
+              .findAny()
+              .orElseThrow(() -> new IOException("Authentication error"));
+      accessToken = service.getAccessToken(extId.email(), secret);
+    } else {
+      throw new IOException("Authentication error");
+    }
     return service.getUserInfo(accessToken);
+  }
+
+  private boolean isAccessToken(String accessToken) {
+    try {
+      JsonObject jsonWebToken = toJsonWebToken(accessToken);
+      return getAttribute(jsonWebToken, USER_NAME_ATTRIBUTE) != null;
+    } catch (IOException e) {
+      return false;
+    }
+  }
+
+  private static String getAttribute(JsonObject json, String name) {
+    JsonPrimitive prim = getAsJsonPrimitive(json, name);
+    return prim != null && prim.isString() ? prim.getAsString() : null;
+  }
+
+  private static JsonPrimitive getAsJsonPrimitive(JsonObject json, String name) {
+    JsonElement attr = json.get(name);
+    if (attr == null || !attr.isJsonPrimitive()) {
+      return null;
+    }
+    return attr.getAsJsonPrimitive();
+  }
+
+  public static JsonObject getAsJsonObject(String s) {
+    JsonElement json = JsonParser.parseString(s);
+    if (!json.isJsonObject()) {
+      return new JsonObject();
+    }
+    return json.getAsJsonObject();
+  }
+
+  private JsonObject toJsonWebToken(String accessToken) throws IOException {
+    String[] segments = getSegments(accessToken);
+    return getAsJsonObject(decodeBase64(segments[1]));
+  }
+
+  private String decodeBase64(String s) {
+    return new String(Base64.getDecoder().decode(s), UTF_8);
+  }
+
+  private String[] getSegments(String accessToken) throws IOException {
+    String[] segments = accessToken.split("\\.");
+    if (segments.length != 3) {
+      throw new IOException("Invalid token: must be of the form 'header.token.signature'");
+    }
+    return segments;
   }
 }
