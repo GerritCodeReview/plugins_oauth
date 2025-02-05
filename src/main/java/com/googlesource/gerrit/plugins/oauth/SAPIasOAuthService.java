06375c8ce24ae3be2a84805315b40d1c17acae3d
Thomas Dräbing <thomas.draebing@sap.com>
2025-02-05 12:29:49 +0100

    [SAP IAS] Set display name to first_name + last_name
    Change-Id: Idb61a520a68b11b488963641dbd5496ec4276473

--- src/main/java/com/googlesource/gerrit/plugins/oauth/SAPIasOAuthService.java
+++ src/main/java/com/googlesource/gerrit/plugins/oauth/SAPIasOAuthService.java
@@ -109,14 +109,22 @@ public class SAPIasOAuthService implements OAuthServiceProvider {
       JsonElement id = jsonObject.get("sub");
       JsonElement username = jsonObject.get("preferred_username");
       JsonElement email = jsonObject.get("email");
-      JsonElement name = jsonObject.get("name");
+      JsonElement firstName = jsonObject.get("first_name");
+      JsonElement lastName = jsonObject.get("last_name");
+      String displayName = "";
+      if (!JsonUtil.isNull(firstName)) {
+        displayName = firstName.getAsString();
+      }
+      if (!JsonUtil.isNull(lastName)) {
+        displayName += " " + lastName.getAsString();
+      }
       return new OAuthUserInfo(
           this.extIdScheme + ":" + id.getAsString() /*externalId*/,
           username == null || username.isJsonNull()
               ? id.getAsString()
               : username.getAsString() /*username*/,
           email == null || email.isJsonNull() ? null : email.getAsString() /*email*/,
-          name == null || name.isJsonNull() ? null : name.getAsString() /*displayName*/,
+          displayName.isBlank() ? null : displayName /*displayName*/,
           linkExistingGerrit ? "gerrit:" + username.getAsString() : null /*claimedIdentity*/);
     } catch (ExecutionException | InterruptedException e) {
       throw new RuntimeException("Cannot retrieve user info resource", e);
