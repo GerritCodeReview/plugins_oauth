7b1379fffb7df956f4d20aa2bfe47b5a2a51fb74
Thomas Dräbing <thomas.draebing@sap.com>
2025-04-23 16:14:32 +0200

    Set OAuth external ID on CreateAccount call
    To create an account you either have to login via the UI or an admin has to
    create it via REST API. The former worked without issue, since OAuth is
    directly involved in account creation, i.e. the data returned by the
    identity provider is being used to set up the account. However, it was not
    possible to create an account via REST API and then log into it via OAuth.
    The reason was that the account created via REST API didn't get an external
    ID on creation that linked it to the IDP. On authentication Gerrit would
    thus complain that the account exists and that the provided secret didn't
    match the stored password instead of contacting the OAuth server.
    
    Gerrit provides an extension point where plugins can provide additional
    external IDs on account creation. This extension point is being implemented
    by this change.
    
    Change-Id: Ie643503f4e1c2247afd1674036aeaf3b33ca7665

--- src/main/java/com/googlesource/gerrit/plugins/oauth/Module.java
+++ src/main/java/com/googlesource/gerrit/plugins/oauth/Module.java
@@ -14,11 +14,42 @@
 
 package com.googlesource.gerrit.plugins.oauth;
 
+import com.google.gerrit.extensions.annotations.Exports;
+import com.google.gerrit.extensions.annotations.PluginName;
+import com.google.gerrit.server.account.AccountExternalIdCreator;
+import com.google.gerrit.server.account.externalids.ExternalIdFactory;
+import com.google.gerrit.server.config.GerritServerConfig;
 import com.google.inject.AbstractModule;
+import com.google.inject.Inject;
+import java.util.List;
+import org.eclipse.jgit.lib.Config;
 
 public class Module extends AbstractModule {
+  private final List<String> configuredProviders;
+  private final ExternalIdFactory externalIdFactory;
+
+  @Inject
+  public Module(
+      @GerritServerConfig Config config,
+      @PluginName String pluginName,
+      ExternalIdFactory externalIdFactory) {
+    configuredProviders =
+        config.getSubsections("plugin").stream()
+            .filter(s -> s.startsWith(pluginName))
+            .map(s -> s.substring(pluginName.length() + 1, s.length() - 6))
+            .toList();
+    this.externalIdFactory = externalIdFactory;
+  }
+
   @Override
   protected void configure() {
     bind(OAuthPluginConfigFactory.class);
+    for (String provider : configuredProviders) {
+      bind(AccountExternalIdCreator.class)
+          .annotatedWith(Exports.named(provider))
+          .toInstance(
+              new OAuthExternalIdCreator(
+                  externalIdFactory, OAuthServiceProviderExternalIdScheme.create(provider)));
+    }
   }
 }
