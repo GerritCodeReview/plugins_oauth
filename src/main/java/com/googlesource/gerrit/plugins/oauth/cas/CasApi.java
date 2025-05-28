61001bb3996869c4b085a73e65c1018115eac373
Thomas Dräbing <thomas.draebing@sap.com>
2025-05-28 10:40:14 +0200

    Sort OAuth implementations into packages
    Change-Id: I3ad158173adb11df6bf313df0dc1e08906d5f1d9

---
+++ src/main/java/com/googlesource/gerrit/plugins/oauth/cas/CasApi.java
@@ -0,0 +1,58 @@
+// Copyright (C) 2016 The Android Open Source Project
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.googlesource.gerrit.plugins.oauth.cas;
+
+import com.github.scribejava.core.builder.api.DefaultApi20;
+import com.github.scribejava.core.extractors.OAuth2AccessTokenExtractor;
+import com.github.scribejava.core.extractors.OAuth2AccessTokenJsonExtractor;
+import com.github.scribejava.core.extractors.TokenExtractor;
+import com.github.scribejava.core.model.OAuth2AccessToken;
+import com.github.scribejava.core.oauth2.bearersignature.BearerSignature;
+import com.github.scribejava.core.oauth2.bearersignature.BearerSignatureURIQueryParameter;
+
+public class CasApi extends DefaultApi20 {
+  private static final String AUTHORIZE_URL = "%s/oauth2.0/authorize";
+
+  private final String rootUrl;
+  private final TokenExtractor<OAuth2AccessToken> tokenExtractor;
+
+  public CasApi(String rootUrl, boolean useJsonExtractor) {
+    this.tokenExtractor =
+        useJsonExtractor
+            ? OAuth2AccessTokenJsonExtractor.instance()
+            : OAuth2AccessTokenExtractor.instance();
+    this.rootUrl = rootUrl;
+  }
+
+  @Override
+  public String getAccessTokenEndpoint() {
+    return String.format("%s/oauth2.0/accessToken", rootUrl);
+  }
+
+  @Override
+  public String getAuthorizationBaseUrl() {
+    return String.format(AUTHORIZE_URL, rootUrl);
+  }
+
+  @Override
+  public BearerSignature getBearerSignature() {
+    return BearerSignatureURIQueryParameter.instance();
+  }
+
+  @Override
+  public TokenExtractor<OAuth2AccessToken> getAccessTokenExtractor() {
+    return tokenExtractor;
+  }
+}
